41ba0817ca2dd818564e8e8b903af8927ed5cb26
田上翔斗 <shoto@stagami.local>
2021-01-24 23:32:28 +0900

    classを拡張する方法について

--- class/js/main.js
+++ class/js/main.js
@@ -17,17 +17,30 @@
     }
   }
 
+  class SponsoredPost {
+    constructor(text, sponsor) {
+      this.text = text;
+      this.likeCount = 0;
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
   const posts = [
     new Post('JavaScriptの勉強中…'),
     new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
   ];
 
-  posts[0].like();
-  // posts[0].likeCount++;
-  // posts[0].likeCount++;
-  // posts[0].likeCount++;
-  // posts[0].likeCount++;
-
-  // posts[0].show();
-  // posts[1].show();
+  posts[2].show();
+  posts[2].like();
 }
